f9ad145d4efd568eb0114c87953a7d1908475978
Pablo <freakaziod210@gmail.com>
2019-06-19 11:11:30 -0600

    Get tests to pass

--- bob.js
+++ bob.js
@@ -4,8 +4,40 @@
 // convenience to get you started writing code faster.
 //
 
-export const hey = (message) => {
-  //
-  // YOUR CODE GOES HERE
-  //
+const responses = {
+    question: "Sure.",
+    yell: "Whoa, chill out!",
+    yellQuestion: "Calm down, I know what I'm doing!",
+    notTalking: "Fine. Be that way!",
+    default: "Whatever."
+};
+
+export const hey = message => {
+    const trimMessage = message.trim();
+    const isYelling = /^[^a-z]*$/.test(trimMessage);
+    const isAsking = /[?]$/.test(trimMessage);
+    const isYellingQuestion = isAsking && isYelling;
+    const isNotTalking = /^[\s]*$/.test(trimMessage);
+    const isNotWords = /^[\d\W]*[^?]$/.test(trimMessage);
+    const isNotWordsQuestion = /^[\d\W]*[?]$/.test(trimMessage);
+
+    if (isNotTalking) {
+        return responses.notTalking;
+    }
+    if (isNotWords) {
+        return responses.default;
+    }
+    if (isNotWordsQuestion) {
+        return responses.question;
+    }
+    if (isYellingQuestion) {
+        return responses.yellQuestion;
+    }
+    if (isAsking) {
+        return responses.question;
+    }
+    if (isYelling) {
+        return responses.yell;
+    }
+    return responses.default;
 };
